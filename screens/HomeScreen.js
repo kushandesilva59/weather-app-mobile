e70ea2b36a5ad8a632873ba6ffa1ee48f402f210
kushandesilva59 <kushandesilva801@gmail.com>
2024-02-18 21:53:09 +0530

    update

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,20 +1,47 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View, Text, Image, SafeAreaView } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { theme } from "../theme";
 
 const HomeScreen = () => {
   return (
     <View className="flex-1 relative">
       <StatusBar style="light" />
 
-      <Text>Hiii</Text>
       <Image
         blurRadius={50}
         source={require("../assets/bg.png")}
         className="absolute h-full w-full"
       />
 
-      
+      <SafeAreaView className="flex flex-1">
+        <View style={{ height: "7%" }} className="mx-4 my-6 relative z-50">
+          <View
+            className="flex-row justify-start items-center rounded-full"
+            style={{ backgroundColor: theme.bgWhite(0.2) }}
+          >
+            <TextInput
+              placeholder="Enter name"
+              placeholderTextColor={"lightgray"}
+              className="pl-2 h-10 text-base text-white"
+            />
+
+            <TouchableOpacity
+              style={{ backgroundColor: theme.bgWhite(0.3) }}
+              className="rounded-full p-3 m-1"
+            >
+              <Text>Icon</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </SafeAreaView>
     </View>
   );
 };
