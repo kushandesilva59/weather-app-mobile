7cde1258d9be5c3e5fc1898f27b9953d24011b71
kushandesilva59 <kushandesilva801@gmail.com>
2024-02-19 20:18:59 +0530

    add images and set dummy weather

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -10,11 +10,15 @@ import {
 } from "react-native";
 import { theme } from "../theme";
 import { MagnifyingGlassIcon } from "react-native-heroicons/outline";
-import {  MapPinIcon } from "react-native-heroicons/solid";
+import { MapPinIcon } from "react-native-heroicons/solid";
 
 const HomeScreen = () => {
   const [showSearch, setShowSearch] = useState(false);
   const [locations, setLocations] = useState([1, 2, 3]);
+
+  const handleLocation = (loc) => {
+    console.log("Location", loc);
+  };
 
   return (
     <View className="flex-1 relative">
@@ -54,20 +58,54 @@ const HomeScreen = () => {
           {locations.length > 0 && showSearch ? (
             <View className="absolute w-full bg-gray-300 top-16 rounded-3xl">
               {locations.map((loc, index) => {
-                let showBorder = index+1 != locations.length;
-                let borderClass = showBorder? 'border-b-2 border-b-gray-400' : ''
+                let showBorder = index + 1 != locations.length;
+                let borderClass = showBorder
+                  ? "border-b-2 border-b-gray-400"
+                  : "";
                 return (
                   <TouchableOpacity
+                    onPress={() => handleLocation(loc)}
                     key={index}
-                    className={"flex-row items-center border-0 p-3 px-4 mb-1 "+borderClass}
+                    className={
+                      "flex-row items-center border-0 p-3 px-4 mb-1 " +
+                      borderClass
+                    }
                   >
-                    <MapPinIcon size='20' color='gray' />
-                    <Text>London, united kingdom</Text>
+                    <MapPinIcon size="20" color="gray" />
+                    <Text className="text-black text-lg ml-2">
+                      London, united kingdom
+                    </Text>
                   </TouchableOpacity>
                 );
               })}
             </View>
           ) : null}
+        </View>
+
+        <View className="mx-4 flex justify-around flex-1 mb-2">
+          <Text className="text-white text-center text-2xl font-bold">
+            London,
+            <Text className="text-lg font-semibold text-gray-300">
+              United Kingdom
+            </Text>
+          </Text>
+
+          <View className="flex-row justify-center">
+            <Image
+              source={require("../assets/sunCloud.png")}
+              className="w-52 h-52"
+            ></Image>
+          </View>
+        </View>
+
+        <View className="space-y-2">
+          <Text className="text-center font-bold text-white text-6xl ml-5">
+            23&#176;
+          </Text>
+
+          <Text className="text-center  text-white text-xl tracking-widest">
+            Partly cloudy
+          </Text>
         </View>
       </SafeAreaView>
     </View>
